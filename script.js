fc9f84a3d908a45631147607712809ee015283a5
aubreyoduori <aubreyoduori@gmail.com>
2026-09-09 10:36:20 +0300

    Add color and background change functions in script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeColor() 
+{ document.getElementById("home").style.color = "red"; 
+} 
+
+
+function makeRed() { document.getElementById("home").style.color = "red"; } function makeBlue() { document.getElementById("home").style.color = "blue"; } function makeGreen() { document.getElementById("home").style.color = "green"; } 
+
+
+
+function yellowBackground() { document.body.style.backgroundColor = "yellow"; } function blueBackground() { document.body.style.backgroundColor = "lightblue"; } function whiteBackground() { document.body.style.backgroundColor = "white"; } 
+
+
+
+
+function darkMode() { document.body.style.backgroundColor = "black"; document.body.style.color = "white"; } 
+
+function lightMode() { document.body.style.backgroundColor = "white"; document.body.style.color = "black"; } 
+
+
+function changeMessage() { document.getElementById("message").innerHTML = "Thank you for visiting our website"; }
